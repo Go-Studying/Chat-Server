2c6c80ac3a816285225ff756f2448aa5f91faa55
junhui <junhuilim@kakao.com>
2026-01-28 13:42:58 +0900

    fix: 불필요한 field 삭제

--- internal/models/chat_room.go
+++ internal/models/chat_room.go
@@ -1,14 +1,10 @@
 package models
 
-import "github.com/google/uuid"
-
 type ChatRoom struct {
 	Base
-	Name      string    `gorm:"size:100" json:"name"`
-	CreatedBy uuid.UUID `gorm:"type:uuid;not null" json:"created_by"`
+	Name string `gorm:"size:100" json:"name"`
 
 	// Relations
-	Creator  User         `gorm:"foreignKey:CreatedBy" json:"-"`
 	Members  []RoomMember `gorm:"foreignKey:RoomID" json:"members,omitempty"`
 	Messages []Message    `gorm:"foreignKey:RoomID" json:"-"`
 }
